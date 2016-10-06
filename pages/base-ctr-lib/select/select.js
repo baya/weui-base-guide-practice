c2d1344f7aa00420e9f4374df11bb6553b34c726
baya <kayak.jiang@gmail.com>
2016-10-06 23:02:58 +0800

    update

--- pages/base-ctr-lib/select/select.js
+++ pages/base-ctr-lib/select/select.js
@@ -12,11 +12,18 @@ Page({
             index: 0,
             array: ['中国', '外国'],
             change: 'bindCountryChange'
-        }
+        },
+        checkRows: [
+            {checkStyle: "display:inline-block", id: 1},
+            {checkStyle: "display:none", id: 2},
+            {checkStyle: "display:none", id: 3}
+        ]
     },
+
     onLoad: function(options){
         this.data.title = options.title
     },
+    
     onReady: function(){
         var title = this.data.title || "调试当前页"
         wx.setNavigationBarTitle( {
@@ -27,7 +34,22 @@ Page({
     bindCountryChange: function(e){
         var country = this.data.country
         country.index = e.detail.value
+        country.picked_country = country.array[country.index]
         this.setData({country: country})
 
+    },
+
+    bindCheckChange: function(e){
+        var checkId = parseInt(e.target.dataset.checkId)
+        var rows = this.data.checkRows
+        rows.forEach(function(row){
+            if(row.id === checkId){
+                row.checkStyle = 'display:inline-block'
+            } else {
+                row.checkStyle = 'display:none'
+            }
+        })
+
+        this.setData({checkRows: rows})
     }
 })
