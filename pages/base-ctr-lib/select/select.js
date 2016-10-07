d572718ff6455a0412bd8b9ceecc4ae4c84542cf
baya <kayak.jiang@gmail.com>
2016-10-07 10:38:42 +0800

    update

--- pages/base-ctr-lib/select/select.js
+++ pages/base-ctr-lib/select/select.js
@@ -1,3 +1,4 @@
+var $sw = require('switcher.js')
 var $image_path = "../../../assets/images/"
 
 Page({
@@ -17,13 +18,24 @@ Page({
             {checkStyle: "display:inline-block", id: 1},
             {checkStyle: "display:none", id: 2},
             {checkStyle: "display:none", id: 3}
+        ],
+
+        switchers: [
+            {id: 1, label: "开启中", checked: "checked", bindchange: "switchChange"},
+            {id: 2, label: "关闭", checked: "", bindchange: "switchChange"}
+        ],
+
+        multCheckRows: [
+            {id:1, icon_type: 'success'},
+            {id:2, icon_type: 'circle'},
+            {id:3, icon_type: 'circle'}
         ]
     },
 
     onLoad: function(options){
         this.data.title = options.title
     },
-    
+
     onReady: function(){
         var title = this.data.title || "调试当前页"
         wx.setNavigationBarTitle( {
@@ -51,5 +63,37 @@ Page({
         })
 
         this.setData({checkRows: rows})
+    },
+
+    switchChange: function(e){
+        var id = parseInt(e.target.dataset.id)
+        var sw_list = this.data.switchers
+        var sw = sw_list.find(function(sw){
+            return sw.id === id
+        })
+        $sw.op(sw, e.detail.value)
+        this.setData( {
+            switchers: sw_list
+        })
+    },
+
+    bindMultCheckChange: function(e){
+        var checkId = parseInt(e.currentTarget.dataset.checkId)
+        var rows = this.data.multCheckRows
+        var row = rows.find(function(row){
+            return row.id === checkId
+        })
+
+        if(row.icon_type === 'success'){
+            row.icon_type = 'circle'
+        } else {
+            row.icon_type = 'success'
+        }
+
+        this.setData({
+            multCheckRows: rows
+        })
     }
+
+   
 })
