ddd5d8de6619167432d88e8e1a279d47d58852c4
baya <kayak.jiang@gmail.com>
2016-10-25 22:13:19 +0800

    update

--- pages/comb-lib/bottom_tab/bottom_tab.js
+++ pages/comb-lib/bottom_tab/bottom_tab.js
@@ -2,6 +2,8 @@ var $image_path = "../../../assets/images/"
 
 Page( {
     data: {
-        tab_item_png: { src: $image_path + "tab_item.png" }
+        tab_item_icon1_png: { src: $image_path + "tab_item_icon1.png" },
+        tab_item_icon2_png: { src: $image_path + "tab_item_icon2.png" },
+        tab_item_icon3_png: { src: $image_path + "tab_item_icon3.png" }
     }
 })
