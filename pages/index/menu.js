2a3adc05972bdf08c76d7d93ef3c3db9f9679739
baya <kayak.jiang@gmail.com>
2016-10-30 22:57:27 +0800

    update

--- pages/index/menu.js
+++ pages/index/menu.js
@@ -33,7 +33,7 @@ const subitem_db = {
         { name: '表单输入组合', nav_url: '../comb-example/form/form' },        
         { name: '表单输入报错',nav_url: '../comb-example/form_error/form_error' },
         { name: '表单输入组合（二）', nav_url: '../comb-example/form2/form2' },
-        { name: '收货地址'},
+        { name: '收货地址', nav_url: '../comb-example/ship_address/ship_address'},
         { name: '新增地址'},
         { name: '结果页'},
         { name: '发送验证码'},
